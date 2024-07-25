8d1f549c8b4cd3667762e4f7391a55c1593d57ae
Atharv Tiwari <atharvtiwari@Atharvs-MacBook-Air.local>
2024-07-25 21:12:10 +0530

    first

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
-console.log("Yadav id lund")
+
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs,not browser
+
+console.log(3+3)
+
+console.log("Atharv")
+
+let name = "Ayush"
+let age = 19
+let isLoggedIn = false
+
+
+
+console.log(typeof undefined); //undifined
+console.log(typeof null); //object
